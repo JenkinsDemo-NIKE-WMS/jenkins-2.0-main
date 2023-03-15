ae5014f9190e1c48298ecece9663844c61176e82
Arjun Teegalapally <arjun.teegalapally@gspann.com>
2023-03-15 16:50:49 +0530

    Final Commit

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -12,7 +12,7 @@ public class DevopsIntegrationApplication {
 
 	@RequestMapping(path="", method = RequestMethod.GET, produces="application/json")
 	public String message(){
-		return "Test Jenkins CI/CD Pipeline Automation Demo";
+		return "Welcome To Jenkins CI/CD Pipeline Automation Demo";
 	}
 
 	public static void main(String[] args) {
